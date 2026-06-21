d3321862ec1a71b4e5bb01e6f21ff51b9cc83a6e
ChristianBarretoP <cbarreto@ime.usp.br>
2026-06-21 12:25:45 -0300

    Graphs of distribution the values

--- eniac/distribution.py
+++ eniac/distribution.py
@@ -1,134 +1,173 @@
+import os
 from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 
-def digit_distribution(train_loader, test_loader):
-    train_counts = Counter()
-    test_counts = Counter()
-    
-    # Train
-    for _, (a_digits, b_digits, _) in train_loader:
-        train_counts.update(a_digits.tolist())
-        train_counts.update(b_digits.tolist())
+# ==============================================
+# CONFIG
+# ==============================================
+DATA_RESULT_PATH = "result"                                 # Result data path
+GRAPH_NAME_DIGIT_DISTRIBUTION = "distribution_digit_graph"  # Distribution digit name
+GRAPH_NAME_SUM_DISTRIBUTION = "distribution_sum_graph"      # Distribution digit name
+GRAPH_NAME_COMBINATION_DIGIT = "combination_digit_graph"    # Distribution digit name
 
-    # Test
-    for _, (a_digits, b_digits, _) in test_loader:
-        test_counts.update(a_digits.tolist())
-        test_counts.update(b_digits.tolist())
+# ==============================================
+# GRAPHS
+# ==============================================
+class Graphs():
+    def __init__(self, root: str, digit_dist: str, sum_dist: str, digit_comb: str, train_loader, test_loader):
+        self.result_dir = root
+        self.digit_dist= digit_dist
+        self.sum_dist = sum_dist
+        self.digit_comb = digit_comb
+        self.train_loader = train_loader
+        self.test_loader = test_loader
 
-    digits = range(10)
+    def digit_distribution(self):
+        train_counts = Counter()
+        test_counts = Counter()
+        
+        # Train
+        for _, (a_digits, b_digits, _) in self.train_loader:
+            train_counts.update(a_digits.tolist())
+            train_counts.update(b_digits.tolist())
 
-    train_values = [train_counts[d] for d in digits]
-    test_values = [test_counts[d] for d in digits]
+        # Test
+        for _, (a_digits, b_digits, _) in self.test_loader:
+            test_counts.update(a_digits.tolist())
+            test_counts.update(b_digits.tolist())
 
-    plt.figure(figsize=(8,5))
+        digits = range(10)
 
-    width = 0.4
-    x = range(10)
+        train_values = [train_counts[d] for d in digits]
+        test_values = [test_counts[d] for d in digits]
 
-    plt.bar([i - width/2 for i in x], train_values,
-            width=width, label="Train")
+        plt.figure(figsize=(8,5))
 
-    plt.bar([i + width/2 for i in x], test_values,
-            width=width, label="Test")
+        width = 0.4
+        x = range(10)
 
-    plt.xticks(x)
-    plt.xlabel("Digit")
-    plt.ylabel("Number of smples")
-    plt.title("Distribution of digit")
-    plt.legend()
+        plt.bar([i - width/2 for i in x], train_values,
+                width=width, label="Train")
 
-    plt.show()
+        plt.bar([i + width/2 for i in x], test_values,
+                width=width, label="Test")
 
-def sum_distribution(train_loader, test_loader):
-    train_sum_counts = Counter()
-    test_sum_counts = Counter()
+        plt.xticks(x)
+        plt.xlabel("Digit")
+        plt.ylabel("Number of smples")
+        plt.title("Distribution of digit")
+        plt.legend()
 
-    for _, (_, _, sums) in train_loader:
-        train_sum_counts.update(sums.tolist())
+        plt.tight_layout()
+        plt.savefig(self.digit_dist, dpi=300, bbox_inches="tight")
+        plt.show()
 
-    for _, (_, _, sums) in test_loader:
-        test_sum_counts.update(sums.tolist())
+    def sum_distribution(self):
+        train_sum_counts = Counter()
+        test_sum_counts = Counter()
 
-    values = range(19)
+        for _, (_, _, sums) in self.train_loader:
+            train_sum_counts.update(sums.tolist())
 
-    train_values = [train_sum_counts[i] for i in values]
-    test_values = [test_sum_counts[i] for i in values]
+        for _, (_, _, sums) in self.test_loader:
+            test_sum_counts.update(sums.tolist())
 
-    plt.figure(figsize=(10,5))
+        values = range(19)
 
-    width = 0.4
-    x = range(19)
+        train_values = [train_sum_counts[i] for i in values]
+        test_values = [test_sum_counts[i] for i in values]
 
-    plt.bar([i - width/2 for i in x], train_values,
-            width=width, label="Train")
+        plt.figure(figsize=(10,5))
 
-    plt.bar([i + width/2 for i in x], test_values,
-            width=width, label="Test")
+        width = 0.4
+        x = range(19)
 
-    plt.xticks(x)
-    plt.xlabel("Sum (y)")
-    plt.ylabel("Number of smples")
-    plt.title("Sum Distribution (y)")
-    plt.legend()
+        plt.bar([i - width/2 for i in x], train_values,
+                width=width, label="Train")
 
-    plt.show()
+        plt.bar([i + width/2 for i in x], test_values,
+                width=width, label="Test")
 
-def digit_combination(train_loader, test_loader):
+        plt.xticks(x)
+        plt.xlabel("Sum (y)")
+        plt.ylabel("Number of smples")
+        plt.title("Sum Distribution (y)")
+        plt.legend()
 
-    train_counts = np.zeros((10, 10), dtype=int)
-    test_counts = np.zeros((10, 10), dtype=int)
+        plt.tight_layout()
+        plt.savefig(self.sum_dist, dpi=300, bbox_inches="tight")
+        plt.show()
 
-    # Train
-    for _, (a_digits, b_digits, _) in train_loader:
-        for a, b in zip(a_digits.tolist(), b_digits.tolist()):
-            train_counts[a, b] += 1
+    def digit_combination(self):
 
-    # Test
-    for _, (a_digits, b_digits, _) in test_loader:
-        for a, b in zip(a_digits.tolist(), b_digits.tolist()):
-            test_counts[a, b] += 1
+        train_counts = np.zeros((10, 10), dtype=int)
+        test_counts = np.zeros((10, 10), dtype=int)
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+        # Train
+        for _, (a_digits, b_digits, _) in self.train_loader:
+            for a, b in zip(a_digits.tolist(), b_digits.tolist()):
+                train_counts[a, b] += 1
 
-    # Train
-    im1 = ax1.imshow(train_counts, cmap="Blues")
-    ax1.set_title("Train")
-    ax1.set_xlabel("Segundo dígito")
-    ax1.set_ylabel("Primer dígito")
-    ax1.set_xticks(range(10))
-    ax1.set_yticks(range(10))
+        # Test
+        for _, (a_digits, b_digits, _) in self.test_loader:
+            for a, b in zip(a_digits.tolist(), b_digits.tolist()):
+                test_counts[a, b] += 1
 
-    for i in range(10):
-        for j in range(10):
-            ax1.text(
-                j, i,
-                train_counts[i, j],
-                ha="center",
-                va="center",
-                color="black"
-            )
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
 
-    # Test
-    im2 = ax2.imshow(test_counts, cmap="Blues")
-    ax2.set_title("Test")
-    ax2.set_xlabel("Segundo dígito")
-    ax2.set_ylabel("Primer dígito")
-    ax2.set_xticks(range(10))
-    ax2.set_yticks(range(10))
+        # Train
+        im1 = ax1.imshow(train_counts, cmap="Blues")
+        ax1.set_title("Train")
+        ax1.set_xlabel("Second digit")
+        ax1.set_ylabel("First digit")
+        ax1.set_xticks(range(10))
+        ax1.set_yticks(range(10))
 
-    for i in range(10):
-        for j in range(10):
-            ax2.text(
-                j, i,
-                test_counts[i, j],
-                ha="center",
-                va="center",
-                color="black"
-            )
+        for i in range(10):
+            for j in range(10):
+                ax1.text(
+                    j, i,
+                    train_counts[i, j],
+                    ha="center",
+                    va="center",
+                    color="black"
+                )
 
-    fig.colorbar(im1, ax=ax1, fraction=0.046)
-    fig.colorbar(im2, ax=ax2, fraction=0.046)
+        # Test
+        im2 = ax2.imshow(test_counts, cmap="Blues")
+        ax2.set_title("Test")
+        ax2.set_xlabel("Second digit")
+        ax2.set_ylabel("First digit")
+        ax2.set_xticks(range(10))
+        ax2.set_yticks(range(10))
 
-    plt.tight_layout()
-    plt.show()
+        for i in range(10):
+            for j in range(10):
+                ax2.text(
+                    j, i,
+                    test_counts[i, j],
+                    ha="center",
+                    va="center",
+                    color="black"
+                )
+
+        fig.colorbar(im1, ax=ax1, fraction=0.046)
+        fig.colorbar(im2, ax=ax2, fraction=0.046)
+
+        plt.tight_layout()
+        plt.savefig(self.digit_comb, dpi=300, bbox_inches="tight")
+        plt.show()
+
+def main_distribution(train_loader, test_loader):
+  # Obtiene el directorio donde está este archivo.py
+  base_dir = os.path.dirname(os.path.abspath(__file__))
+  # Une el directorio de base_dir con las carpetas "data" y "result"
+  result_dir = os.path.join(base_dir, DATA_RESULT_PATH)
+  digit_dist = os.path.join(result_dir, f"{GRAPH_NAME_DIGIT_DISTRIBUTION}.png")
+  sum_dist = os.path.join(result_dir, f"{GRAPH_NAME_SUM_DISTRIBUTION}.png")
+  digit_comb = os.path.join(result_dir, f"{GRAPH_NAME_COMBINATION_DIGIT}.png")
+  graph = Graphs(result_dir, digit_dist, sum_dist, digit_comb, train_loader, test_loader)
+  graph.digit_distribution()
+  graph.sum_distribution()
+  graph.digit_combination()
